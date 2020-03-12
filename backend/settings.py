7e70dc5e4d014a6f8baaf05c73a74db304c35b8d
akithegood <a.saukoniemi@gmail.com>
2020-03-12 22:12:55 +0200

    Added Windows Procfile

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'enigmatic-savannah-66471.herokuapp.com']
 
 
 # Application definition
